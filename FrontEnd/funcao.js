2d76cdc76e092b75b5102e5100c0ba2dacdc6592
Fábio Vieira <43624576+eletricfabio@users.noreply.github.com>
2019-01-28 22:24:02 -0200

    Add files via upload

---
+++ FrontEnd/funcao.js
@@ -0,0 +1,17 @@
+//alert("Bem-Vindo a minha página!");
+
+window.onload = function() {
+
+	var img = document.querySelector('img');
+	img.addEventListener('click', function() {
+		alert('funcionando');
+	});
+
+	// var img = document.querySelector("img");
+	// img.addEventListener("click", function() {
+	// 	console.log('asd');
+	// 	alert("Olá");
+	// });
+
+	// body...
+}
